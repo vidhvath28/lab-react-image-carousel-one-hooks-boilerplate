cf24dacb2bcc70f648277cf03ea8bc85169290c8
vidhvath28 <vidhvathj.2005@gmail.com>
2023-12-17 10:45:18 +0530

    update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,31 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [curIndex,setIndex]=useState(0);
+
+    const goToPrevSlide=()=>{
+        setIndex((prev)=>(prev-1+images.length)%images.length);
+    }
+    const goToNextSlide=()=>{
+        setIndex((prev)=>(prev+1)%images.length)
+
+    }
+
+    return(
+        <>
+            <div className="carousel-container">
+                <ArrowBackIosIcon className="arrow-icon" onClick={goToPrevSlide} />
+                <div className="carousel-content">
+                    <h2>{images[curIndex].title}</h2>
+                    <p>{images[curIndex].subtitle}</p>
+                    <img src={images[curIndex].img} alt={images[curIndex].title} />
+
+                </div>
+                <ArrowForwardIosIcon className="next" onClick={goToNextSlide} />
+
+            </div>
+        </>
+    )
 }
 
 export default Carousel;
